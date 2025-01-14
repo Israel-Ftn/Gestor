7b231e1fce3d956ae0a87968ccfce4368d6d5654
Israel Fiorentini <israelfiorentini189@gmail.com>
2025-01-14 17:08:27 -0300

    finalizando a estrutura Html

--- src/app/lista-de-tarefas/lista-de-tarefas.component.ts
+++ src/app/lista-de-tarefas/lista-de-tarefas.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ListaDetalhesComponent } from "../lista-detalhes/lista-detalhes.component";
+import { FiltroComponent } from "../filtro/filtro.component";
 
 @Component({
   selector: 'app-lista-de-tarefas',
   standalone: true,
-  imports: [],
+  imports: [ListaDetalhesComponent, FiltroComponent],
   templateUrl: './lista-de-tarefas.component.html',
   styleUrl: './lista-de-tarefas.component.css'
 })
